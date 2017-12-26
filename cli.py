32e3c2eb7a4b030fc4ec4b05ceb5c53446df6859
Rodrigo Sampler <rodrigoufcquixada@gmail.com>
2017-12-26 00:35:20 -0300

    correção erro repetir dados

--- cli.py
+++ cli.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import core
 import sys
 import csv
+import os
 
 
 import time
@@ -12,18 +13,24 @@ import time
 
 
 if __name__ == "__main__":
-    _estacoes = core._busca(sys.argv[1]) if len(sys.argv) > 1 else core._get()
-    i=0
-    with open("DadosBicicletarUT.csv", "wb") as csvfile:
-        spamwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-        print "PROJETO BICICLETAR - FORTALEZA/BR"
-        while True:
-            for _estacao in _estacoes:
-                now = datetime.now()
-                data= now.strftime('%Y-%m-%d %H:%M:%S')
-                _nome, _lat, _long, _endereco, _linha, _statusOnline, _StatusOperacional, _disp1, _disp2, _total, _internalStatus, _img, _id = _estacao
-                spamwriter.writerow([data, _id, _nome.encode('utf8'), _lat, _long, _linha.encode('utf8'), _endereco.encode('utf8'), _statusOnline, _StatusOperacional, _internalStatus, _disp1, _disp2, _total])
-                #print 'estacao id %d:' % _id, _nome, '-', _linha, '-', _endereco, ' / bikes disponiveis: %d, vagas livres: %d' % (int(_disp2),int(_total))
-                #i+=1
-            #print 'foram mostradas %d estacoes' % i
-            time.sleep(60)
+    while True:
+        _estacoes = core._busca(sys.argv[1]) if len(sys.argv) > 1 else core._get()
+
+        for _estacao in _estacoes:
+            now = datetime.now()
+            data= now.strftime('%Y-%m-%d %H:%M:%S')
+            minha_data = data[:10]
+            _nome, _lat, _long, _endereco, _linha, _statusOnline, _StatusOperacional, _disp1, _disp2, _total, _internalStatus, _img, _id = _estacao
+
+            nome_arquivo = minha_data+'/ESTACAO_'+str(_id)+'.csv'
+            if not os.path.exists(minha_data):
+                os.makedirs(minha_data)
+
+            with open(nome_arquivo, "a") as csvfile:
+                try:
+                    spamwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+                    spamwriter.writerow([data, _id, _nome, _lat, _long, _linha, _endereco, _statusOnline, _StatusOperacional, _internalStatus, _disp1, _disp2, _total])
+                finally:
+                    csvfile.close()
+        print("proximo")
+        time.sleep(60)
